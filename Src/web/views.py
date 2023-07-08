82685e2a2559fea4e8ec59892827da005171e5b9
Zhong John Li <zhong.li.8432@gmail.com>
2023-07-08 12:00:38 -0400

    add missing modified files

--- Src/web/views.py
+++ Src/web/views.py
@@ -6,10 +6,10 @@ def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        #user = username
+        #user = authenticate(request, username=username, password=password)
+        user = username
         if user is not None:
-            login(request, user)
+            #login(request, user)
             return redirect('home')  # Replace 'home' with the URL name of your home page
         else:
             messages.error(request, 'Invalid username or password.')
